e253575dcb0dd8b22a410b3104027aa19c1489eb
Onkar Divekar <onkardivekar07@gmail.com>
2024-01-19 09:40:10 +0530

    M1 Task-7

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>Expense Item!</h2>
+      <p>Food Rs 10</p>
+      <p>petrol RS 200</p>
+      <p>Movies RS 200</p>
+    </div>
+  );
+}
+
+export default ExpenseItem;
